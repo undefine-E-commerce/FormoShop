4f8bc851b3c7775df6540041cc4f767487b64997
FacundoMajda <facundomajda13@gmail.com>
2023-10-29 23:04:56 -0300

    -client

--- client/src/Components/UserManag/Login.jsx
+++
@@ -1,8 +0,0 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
-
-export const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <button className="btn btn-md btn-light" onClick={() => loginWithRedirect()}>Log In</button>;
-};
